5573ac9945b4c37d4de00c3044549f98534a470e
Wallace R C Muniz <wallacemuniz@hotmail.com>
2021-07-03 09:14:23 -0300

    [NEW] Commit modulo COVID

---
+++ meus_modulos/covid_19/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Wallace R C Muniz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{    'name': 'Covid 19',
+    'description': """
+        Treinamento Kmee - Projeto 1 - Análises Covid 19""",
+    'version': '1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Wallace R C Muniz',
+    'website': 'www.devtech.inf.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
